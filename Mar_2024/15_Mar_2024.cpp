e02018f489592a3b42578dd78c9d370f5b95e289
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-03-15 17:41:34 +0530

    Update 15_Mar_2024.cpp

--- Mar_2024/15_Mar_2024.cpp
+++ Mar_2024/15_Mar_2024.cpp
@@ -1,71 +1,83 @@
 class Solution
 {
     public:
-    
-    Node* merge(Node *head1, Node* head2){
-        if(!head1) return head2;
-        if(!head2) return head1;
-        
-        Node* temp=head1;
-        while(temp->next){
-            temp=temp->next;
-        }
-        temp->next=head2;
-        
-        return head1;
-    }
-    
-    void reverse(Node *&head){
-        Node* prev=NULL, *curr=head, *next;
-        
-        while(curr){
-            next=curr->next;
-            curr->next= prev;
-            prev = curr;
-            curr=next;
-        }
-        head=prev;
-    }
-    
-    void splitList(Node*head, Node **Ahead, Node**Dhead){
-        *Ahead=new Node(0);
-        *Dhead=new Node(0);
-        
-        Node*ascn= *Ahead;
-        Node*dscn= *Dhead;
-        Node* curr=head;
-        while(curr){
-            ascn->next=curr;
-            ascn=ascn->next;
-            curr=curr->next;
-            
-            if(curr){
-                dscn->next=curr;
-                dscn=dscn->next;
-                curr=curr->next;
-            }
-        }
-        
-        ascn->next=NULL;
-        dscn->next=NULL;
-        
-        *Ahead=(*Ahead)->next;
-        *Dhead=(*Dhead)->next;
-        
-        return;
-    }
-    
     // your task is to complete this function
+    void reverse(Node **head){
+        Node *pre = NULL,*cur = *head;
+        while(cur){
+            Node *next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        *head = pre;
+    }
+    
+    Node* merge(Node **r1, Node **r2){
+        if(*r1==NULL){
+            return *r2;
+        }
+        if(*r2 == NULL){
+            return *r1;
+        }
+        Node *r = new Node(0);
+        Node *ptr = r;
+        while(*r1 and *r2){
+            if((*r1)->data<(*r2)->data){
+                ptr->next = *r1;
+                *r1 = (*r1)->next;
+            }
+            else{
+                ptr->next = *r2;
+                *r2 = (*r2)->next;
+            }
+            ptr = ptr->next;
+        }
+        
+        while(*r2){
+            ptr->next = (*r2);
+            ptr = ptr->next;
+            *r2 = (*r2)->next;
+        }
+        while(*r1){
+            ptr->next = (*r1);
+            ptr = ptr->next;
+            *r1 = (*r1)->next;
+        }
+        
+        if(ptr){
+            ptr->next = NULL;
+        }
+        return r->next;
+    }
+    
     void sort(Node **head)
     {
-         // Code here
-         Node*Ahead, *Dhead;
-         splitList(*head, &Ahead, &Dhead);
-         
-         reverse(Dhead);
-         
-         *head=merge(Ahead, Dhead);
-         
-         return;
+        Node *r1 = new Node(0);
+        Node *r2 = new Node(0);
+        Node * ptr = *head;
+        Node *f = r1,*s = r2;
+        while(ptr){
+            f->next = ptr;
+            ptr = ptr->next;
+            f = f->next;
+            
+            if(ptr){
+                s->next = ptr;
+                s = s->next;
+                ptr = ptr->next;
+            }
+        }
+        if(f){
+            f->next = NULL;
+        }
+        if(s){
+            s->next = NULL;
+        }
+        
+        reverse(&(r2->next));
+        
+        *head = merge(&(r1->next),&(r2->next));
+        
     }
 };
