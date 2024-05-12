27480675c5d3c1a1f2133de3ec27112166029e2f
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-05-12 23:16:50 +0530

    Create 12_May_2024.cpp

---
+++ May_2024/12_May_2024.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    int minSteps(int d) {
+        queue<int> q;
+        q.push(0);
+        int l = 1;
+        while(true){
+            int n = q.size();
+            unordered_set<int> pushed;
+            for(int i = 0; i < n; i++){
+                auto f = q.front();
+                q.pop();
+                int a = f + l, b = f + (-1)*l;
+                if(a == d or b == d){
+                    return l;
+                }
+                if(!pushed.count(a)){
+                    q.push(a);
+                    pushed.insert(a);
+                }
+                if(!pushed.count(b)){
+                    q.push(b);   
+                    pushed.insert(b);
+                }
+            }
+            l++;
+        }
+        return -1;
+    }
+};
