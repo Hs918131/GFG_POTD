7e9269adbc5eadd0dedaa7b01f6f9472e89ea5a7
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-07-29 22:21:56 +0530

    Create 29_July_2024.cpp

---
+++ July 2024/29_July_2024.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int> > &arr) {
+        // code here
+        int n = arr.size();
+        int m = arr[0].size();
+        int j;
+        int cnt=0;
+        int maxi=INT_MIN;
+        int row = -1;
+        for(int i=0;i<n;i++)
+        {
+            j=0;
+            while(arr[i][j]!=1 && j<m)
+            {
+                j++;
+            }
+            if(m-j > maxi  && j!=m)
+            {
+                maxi = m-j;
+                row = i;
+            }
+            
+        }
+        return row;
+    }
+};
