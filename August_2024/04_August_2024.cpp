1f3108ad69c92439ec35476c13908490c84b5ad2
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-08-04 21:59:12 +0530

    Create 04_August_2024.cpp

---
+++ August_2024/04_August_2024.cpp
@@ -0,0 +1,19 @@
+int maxMeetings(int n, int start[], int end[]) {
+        vector<pair<int,int>> v;
+        int count=1;
+        for(int i=0;i<n;i++)
+        {
+            v.push_back({end[i],start[i]});
+        }
+        sort(v.begin(),v.end());
+        int last=v[0].first;
+        for(int i=1;i<n;i++)
+        {
+            if(last<v[i].second)
+            {
+                count++;
+                last=v[i].first;
+            }
+        }
+        return count;
+    }
