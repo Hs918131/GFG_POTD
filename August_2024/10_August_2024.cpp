f36e9d2ec8e3861192247f886d2993ae120ab480
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-08-10 21:56:01 +0530

    Create 10_August_2024.cpp

---
+++ August_2024/10_August_2024.cpp
@@ -0,0 +1,25 @@
+Node* rotate(Node* head, int k) {
+        // Your code here
+        if(head==NULL || head->next==NULL)
+        {
+            return head;
+        }
+        Node*temp=head;
+        Node*ptr=head;
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        
+        while(k!=0)
+        {
+            head=head->next;
+            ptr->next=NULL;
+            temp->next=ptr;
+            temp=ptr;
+            ptr=head;
+            k--;
+        }
+        return head;
+        
+    }
